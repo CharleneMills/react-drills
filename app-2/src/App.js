6486546b7e5b4f3af034d42e427350843bbc0ff9
CharleneMills <charleneskater@gmail.com>
2020-05-13 14:58:50 -0600

    App 2 Done

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
+import React, { Component } from "react";
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  
+  constructor() {
+    super();
+
+    this.state = {
+      colors: ['green', 'blue', 'yellow', 'red', 'purple', 'orange']
+    }
+
+  }
+  
+
+  render() {
+
+    let colors = this.state.colors.map( (el, index) => {
+        return <h2 style={{color: el}} key={index}>{el}</h2>;
+    });
+
+
+    return (
+      <div className="App">
+        <div className="App-body">
+          <div className="container">
+            {colors}
+          </div>  
+        </div>
+      </div>
+    );
+  }
+} 
 
 export default App;
